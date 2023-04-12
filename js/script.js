bfa2b8ad2bbb02b3c28d8ebe48cc2d581aaff04d
jgj9805 <106056571+jgj9805@users.noreply.github.com>
2023-04-12 18:45:06 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
+// 헤더 리스트 내려오게 하기, 헤더 스크롤 시 고정
 $(this).on({
     scroll: function (e) {
-        /* S GNB fixed */
         var headerOffsetT = $('.header').offset().top;
         var headerOuterH = $('.header').outerHeight(true);
         var fixedHeaderPosY = headerOffsetT + headerOuterH;
@@ -34,6 +34,33 @@ $(document).ready(function () {
         }
     });
 });
+
+//메인 동영상 재생, 일시정지
+function video_pause() {
+    const play = document.getElementById("main_pause").innerHTML
+    if(play == "play_arrow"){
+        document.getElementById("main_video").play();
+        document.getElementById("main_pause").innerHTML = "pause"
+    }
+    else if(play == "pause"){
+        document.getElementById("main_video").pause();
+        document.getElementById("main_pause").innerHTML = "play_arrow"
+    }
+}
+
+//메인 동영상 사운드 재생, 음소거
+function video_mute() {
+    const play = document.getElementById("main_sound").innerHTML
+    if(play == "volume_off"){
+        document.getElementById("main_video").muted = true;
+        document.getElementById("main_sound").innerHTML = "volume_up"
+    }
+    else if(play == "volume_up"){
+        document.getElementById("main_video").muted = true;
+        document.getElementById("main_sound").innerHTML = "volume_off"
+    }
+}
+    
 
 
 
